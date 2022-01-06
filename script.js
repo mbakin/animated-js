331fc5f90b0719d45af7fa2f31086666f7d2a2f4
mbakin <mehmetberkakinn@outlook.com>
2022-01-06 17:10:47 +0300

    initial script and create elements

---
+++ script.js
@@ -0,0 +1,6 @@
+/** @type {HTMLCanvasElement} */
+
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
